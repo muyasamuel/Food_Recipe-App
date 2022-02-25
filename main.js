e0316b8b3a1910b320760ad711598f59cf0da5ce
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-02-25 19:09:40 +0300

    fetching recipe api through the url

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ const searchBtn = document.querySelector(".search-btn")
 
 const app_id = 'c36d6e6d';
 const app_key= 'e4b9dd0d518a6204a9eb47f8443e4063';
+ 
 
 searchBtn.addEventListener('click', (e)=>{
    e.preventDefault();
    let inputtedValue = formInput.value;
-   console.log(inputtedValue)
+   fetch(`https://api.edamam.com/search?q=pizza&app_id=${app_id}&app_key=${app_key}`)
+   .then(Response => Response.json() )
+   .then(data => console.log(data))
+ 
 
 })
